33b5f9a8b2faa574c7b0b97f885a51edbca6237d
Hardik Sahu <sahuhardic@gmail.com>
2023-01-27 23:00:55 +0530

    removed some files

--- client/portal/provider/job/ProviderCombinedLine.ts
+++
@@ -1,9 +0,0 @@
-import { VendorOrderAccountingLineFragment } from 'shared/generated';
-
-export default interface ProviderCombinedLine {
-  id: string;
-  revenue?: VendorOrderAccountingLineFragment;
-  fieldId?: string;
-  fieldName?: string;
-  fieldValue?: string;
-}
